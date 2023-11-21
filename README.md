a95333b304401ad1d095c70e88f2a246cb74923c
chitongyu <chitongyu@gmail.com>
2023-11-21 15:56:32 +0800

    修改readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# readme
+
+## 在git中执行
+
+```js
+
+bash deploy.sh
+
+```
+
+## github pages搭建
+
+[VuePress + Github Pages 搭建博客](https://juejin.cn/post/7041134607869149215)
---
+++ src/.vuepress/config.ts
@@ -0,0 +1,32 @@
+import { defineUserConfig } from "vuepress";
+import { searchProPlugin } from "vuepress-plugin-search-pro";
+import theme from "./theme.js";
+
+export default defineUserConfig({
+  base: "/silverhorn/",
+
+  locales: {
+    "/": {
+      lang: "en-US",
+      title: "Blog Demo",
+      description: "A blog demo for vuepress-theme-hope",
+    },
+    "/zh/": {
+      lang: "zh-CN",
+      title: "博客演示",
+      description: "vuepress-theme-hope 的博客演示",
+    },
+  },
+
+  theme,
+
+  // Enable it with pwa
+  // shouldPrefetch: false,
+  plugins: [
+    searchProPlugin({
+      // 配置选项
+      indexContent: true, // 配置索引正文的内容
+      autoSuggestions: true, // 自动提示搜索建议
+    }),
+  ],
+});
